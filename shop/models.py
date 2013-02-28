2ad7879107a37ceded85b8440468b80684d8036e
Michael Xia <xia.umd@gmail.com>
2013-02-27 19:58:01 -0500

    updated models

--- shop/models.py
+++ shop/models.py
@@ -1,36 +1,37 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
-class shipping_label(models.Model):
+class ShippingLabel(models.Model):
 	address_first_line = models.CharField(max_length=50)
 	address_second_line = models.CharField(max_length=50)
 	address_zip_code = models.IntegerField()
 	address_city = models.CharField(max_length=50)
 	address_state = models.CharField(max_length=2)
-	owner = models.OneToOneField(customer)
 
-class credit_card(models.Model):
+class CreditCard(models.Model):
 	cc_stub = models.CharField(max_length=4, default="****")
+	cc_type = models.IntegerField() 
 
-class categories(models.Model):
+class Categories(models.Model):
+	category_thumb = models.CharField(max_length=200)
 	category_name = models.CharField(max_length=50)
 
-class item(models.Model):
+class Item(models.Model):
 	item_name = models.CharField(max_length = 50)
-	item_price = models.IntegerField(efault = 99999)
+	item_price = models.IntegerField(default = 99999)
 	item_size = models.CharField(max_length=50)
 	item_qty = models.IntegerField(default=0)
+	item_thumb = models.CharField(max_length=200)
 
-class shopping_cart(models.Model):
-	items = models.ForeignKey(item)
-	owner = models.OneToOneField(customer)
+class ShoppingCart(models.Model):
+	items = models.ForeignKey(Item)
 
-class customer(models.Model):
-	current_shipping_addr = models.ForeignKey(shipping_label)
-	current_creditcard = models.OneToOneField(credit_card)
+class Customer(models.Model):
+	current_shipping_addr = models.ForeignKey(ShippingLabel)
+	current_creditcard = models.OneToOneField(CreditCard)
 	stripe_token = models.CharField(max_length=20)
-	cart = models.OneToOneField(shopping_cart)
+	cart = models.OneToOneField(ShoppingCart)
 	phone_number = models.CharField(max_length=20)
 	delivery_time = models.DateTimeField()
-	user = models.OneToOneField(User, primary_key=True)
+	user = models.OneToOneField(User)
