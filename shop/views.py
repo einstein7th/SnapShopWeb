22486536c32abdaa689521e50d8170d1c7eaae56
Eddie Xue <drpizza.x@gmail.com>
2013-03-05 00:45:05 -0500

    Fix a few {% url 'name' %}  bugs, clean up views file

--- shop/views.py
+++ shop/views.py
@@ -1,18 +1,16 @@
 from django.shortcuts import render_to_response
 from django.shortcuts import render
-from django.template import RequestContext
-from shop.models import ShopItem, Categories
 from django.shortcuts import HttpResponseRedirect
-from snapshop.shop.models import PurchaseForm
-from snapshop.shop.models import RegisterForm
+from django.template import RequestContext
 from django.contrib.auth.models import User
-from django.forms.util import ErrorList
-
-import stripe
-
 from django.contrib.auth import *                                       # django.contrib.auth.login is internal method used to log in user
 from django.contrib.auth import login as authLogin
-from django.contrib.auth.views import login             # django.contrib.auth.views.login renders /accounts/login.html view
+from django.forms.util import ErrorList
+
+from snapshop.shop.models import PurchaseForm
+from snapshop.shop.models import RegisterForm
+from shop.models import ShopItem, Categories
+import stripe
 
 stripe.api_key = 'sk_test_sdufAGHnjkSHOAO17JLy7mnT' # Test secret key
 # 'sk_live_rytd9V25kvaLa0THdVIlLwae' # Live secret key
@@ -47,57 +45,6 @@ def search(request):
 
 def thanks(request):
     return render_to_response("thanks.html",{},RequestContext(request))
-
-# def main(request):
-#     if request.method == 'POST': # Submitted the purchase form:
-#         form = PurchaseForm(request.POST)
-#         print 'checking form validity'
-
-#         if form.is_valid():
-#             card_dictionary = {
-#                 'number': form.cleaned_data['card_number'],
-#                 'exp_month': form.cleaned_data['card_expiration_month'],
-#                 'exp_year': form.cleaned_data['card_expiration_year'],
-#                 'cvc': form.cleaned_data['card_cvc'],
-#                 'address_zip': form.cleaned_data['card_zip'],
-#             }
-
-#             # process order
-#             charge = stripe.Charge.create(
-#                 amount=1337,
-#                 currency='usd',
-#                 card=card_dictionary,
-#                 description='SnapShop order for ' + form.cleaned_data['name'],
-#                 )
-
-#             # Try only creating customer:
-#             customer = stripe.Customer.create(
-#                 # use SnapShop username?
-#                 description="Customer " + form.cleaned_data['name'],
-#                 card=card_dictionary,
-#                 account_balance=1337,
-#                 )
-
-#             print charge
-
-#             if charge.paid: # if succeeds then we go to thank you page
-#                 # send email
-#                 return HttpResponseRedirect('/thanks/')
-
-#             # if invalid proceeds below, and re-displays form showing errors
-#             else:
-#                 form.errors = c.failure_message
-
-#     else:
-#         form = PurchaseForm()
-
-#     query = request.GET.get("q","")
-#     keywords = query.split(" ") if query else None
-
-#     return render_to_response("main.html",
-#                               {'query': query,
-#                                'keywords': keywords,
-#                                'form': form},
 
 def results(request):
     if request.method == 'POST': # Submitted the purchase form:
@@ -142,7 +89,6 @@ def results(request):
     else:
         form = PurchaseForm()
 
-
     query = request.GET.get("q","")
     keyword_item_map = {}
     for keyword in query.split(" "):
@@ -156,4 +102,4 @@ def results(request):
                               {'query':query,
                                'keyword_item_map':keyword_item_map,
                                'form': form},
-                              RequestContext(request))
+                               RequestContext(request))
